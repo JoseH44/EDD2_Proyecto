8b2ca5199c4a8735fb30a713c61f26e85987b3bd
Jose <jrdjhc45@gmail.com>
2020-12-07 18:54:36 -0600

    Clases agregadas: AVL, Node y Data

---
+++ EDD2_Proyecto_/src/edd2_proyecto_/Data.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edd2_proyecto_;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author jrdjh
+ */
+public class Data {
+    int key;
+    long ubicacion;
+    String marcado;
+    ArrayList<Object> datos = new ArrayList();
+
+    
+
+    public Data() {
+        this.marcado = "|";
+    }
+
+    public int getKey() {
+        return key;
+    }
+
+    public void setKey(int key) {
+        this.key = key;
+    }
+
+    public long getUbicacion() {
+        return ubicacion;
+    }
+
+    public void setUbicacion(long ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+
+    public String getMarcado() {
+        return marcado;
+    }
+
+    public void setMarcado(String marcado) {
+        this.marcado = marcado;
+    }
+
+    public ArrayList<Object> getDatos() {
+        return datos;
+    }
+
+    public void setDatos(ArrayList<Object> datos) {
+        this.datos = datos;
+        key = (int)datos.get(0);
+    }
+    
+    
+    
+    
+    
+    
+}
