9b513ac7d924ba8da6a0c2e908a3e886cdf1efff
sergios1001 <sergiosb1001@gmail.com>
2020-12-14 21:34:02 -0600

    lib excel

--- EDD2_Proyecto_/src/edd2_proyecto_/Excel.java
+++ EDD2_Proyecto_/src/edd2_proyecto_/Excel.java
@@ -13,6 +13,10 @@ import java.util.Set;
 import java.util.TreeMap;
 import javax.swing.JTable;
 
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
 /**
  *
  * @author Workstation DSY
@@ -40,11 +44,6 @@ public class Excel {
             }
             data.put(Integer.toString(i + 2), Registro.toArray());
         }
-        //data.put("1", new Object[] {"ID", "NAME", "LASTNAME"});
-        //data.put("2", new Object[] {1, "Amit", "Shukla"});
-        //data.put("3", new Object[] {2, "Lokesh", "Gupta"});
-        //data.put("4", new Object[] {3, "John", "Adwards"});
-        //data.put("5", new Object[] {4, "Brian", "Schultz"});
 
         //Iterate over data and write to sheet
         Set<String> keyset = data.keySet();
