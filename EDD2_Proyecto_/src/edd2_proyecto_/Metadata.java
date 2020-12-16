bfaf3b3094673888635fb676c1ca553a66c90e20
sergios1001 <sergiosb1001@gmail.com>
2020-12-16 15:57:16 -0600

    clase metadata

---
+++ EDD2_Proyecto_/src/edd2_proyecto_/Metadata.java
@@ -0,0 +1,119 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edd2_proyecto_;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Workstation DSY
+ */
+public class Metadata {
+    private String nombre;
+    private ArrayList campos;
+    private ArrayList tipos;
+    private int numregistros = 0;
+    //private boolean estado; // True significa que hay informacion en la metadata.
+    private long Campos_en_Archivo;
+    Btree ArbolB;
+    private int sizeMeta;
+
+    public Btree getArbolB() {
+        return ArbolB;
+    }
+
+    public void setArbolB(Btree ArbolB) {
+        this.ArbolB = ArbolB;
+    }
+
+    public Metadata() {
+       // estado = false;
+       ArbolB = new Btree();
+    }
+
+    public int getSizeMeta() {
+        return sizeMeta;
+    }
+
+    public void setSizeMeta(int sizeMeta) {
+        this.sizeMeta = sizeMeta;
+    }
+    
+
+    public ArrayList getTipos() {
+        return tipos;
+    }
+
+    public void setTipos(ArrayList tipos) {
+        this.tipos = tipos;
+
+    }
+
+    
+
+ 
+
+    public int getNumregistros() {
+        return numregistros;
+    }
+
+    public void setNumregistros(int numregistros) {
+        this.numregistros = numregistros;
+    }
+
+    public void addnumregistros() {
+        System.out.println("ADDING ON METADATA!");
+        this.numregistros++;
+    }
+    public void subtractnumregistros(){
+        System.out.println("SUBTRACTING ON METADAT.");
+        this.numregistros--;
+    }
+
+    public ArrayList getCampos() {
+        return campos;
+    }
+
+    public void setCampos(ArrayList campos) {
+        this.campos = campos;
+
+    }
+
+    public Metadata(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public long getCampos_en_Archivo() {
+        return Campos_en_Archivo;
+    }
+
+    public void setCampos_en_Archivo(long Campos_en_Archivo) {
+        this.Campos_en_Archivo = Campos_en_Archivo;
+    }
+    
+
+    @Override
+    public String toString() {
+        return "Metadata{" + "nombre=" + nombre + '}';
+    }
+
+
+
+
+
+
+
+
+
+}
